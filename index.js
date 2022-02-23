3b38711e7b4e8f2d0f8b1c20a661ba72f5c95ef4
shapanmiah <shapanmiah19@gmail.com>
2022-02-24 02:57:20 +0600

    server update

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ client.connect(err => {
   // })
 
   app.get('/appointmentsByDateEmail', async(req, res) => {
-    const date = req.query.date;
+    // const date = req.query.date;
     const email = req.query.email;
-    const query = {email: email, date: date};
+    const query = {email: email};
     await appointmentsCollection.find(query)
     .toArray((err, appointmentsByDateEmail) => {
       res.send(appointmentsByDateEmail)
